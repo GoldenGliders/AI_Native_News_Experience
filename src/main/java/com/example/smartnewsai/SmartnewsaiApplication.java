613b91834d0f982db58ba7d54d358f6ba2cb3bad
Chirashri Karmakar <chirashrikarmakar345@gmail.com>
2026-03-26 12:04:09 +0530

    commit

---
+++ src/main/java/com/example/smartnewsai/SmartnewsaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartnewsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnewsaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartnewsaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartnewsai/SmartnewsaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartnewsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartnewsaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
