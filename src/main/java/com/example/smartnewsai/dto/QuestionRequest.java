21e564b641139fe4497b4af85d52e3d20967ae83
tanusree-sadhukhan <SADHUKHANTANUSREE7@GMAIL.COM>
2026-03-27 18:40:43 +0530

    commit

---
+++ src/main/java/com/example/smartnewsai/dto/QuestionRequest.java
@@ -0,0 +1,16 @@
+package com.example.smartnewsai.dto;
+
+public class QuestionRequest {
+
+    private String question;
+
+    public QuestionRequest() {}
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
