9f5c84dbfd17a8eaa2165703edab837b8e544369
tanusree-sadhukhan <SADHUKHANTANUSREE7@GMAIL.COM>
2026-03-27 18:20:19 +0530

    Script
    script

---
+++ src/main/java/com/example/smartnewsai/model/News.java
@@ -0,0 +1,50 @@
+package com.example.newsai.model;
+
+public class News {
+
+    private String title;
+    private String content;
+    private String category;
+    private String date;
+
+    public News() {}
+
+    public News(String title, String content, String category, String date) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
