80c74ed93b3bd593c177c123ce5744521b5cf61d
Chirashri Karmakar <chirashrikarmakar345@gmail.com>
2026-03-28 14:18:30 +0530

    commit

--- src/main/java/com/example/smartnewsai/model/FlashCard.java
+++ src/main/java/com/example/smartnewsai/model/FlashCard.java
@@ -1,5 +1,30 @@
-package com.example.smartnewsai.model;
+package com.example.newsai.model;
 
 public class FlashCard {
-    
+
+    private String question;
+    private String answer;
+
+    public FlashCard() {}
+
+    public FlashCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
